32d7072684805a6b8609aa0f3b991ff47df917b1
Scrirock <73163680+Scrirock@users.noreply.github.com>
2020-12-04 10:27:45 +0100

    ghjkhgh

--- exo 1/exercice1.js
+++ exo 1/exercice1.js
@@ -1,9 +1,12 @@
 //Votre code ci dessous
 
+let cat = document.getElementById("cat-pic");
 
+function carresse() {
+    document.getElementById("cat-chat").innerHTML = "'Miaou', ou tout ce que vous supposez qu'un chat exprimerait si on le caressait.";
+}
 
-
-
+addEventListener("click",  carresse)
 
 
 
--- exo 3/exercice.js
+++ exo 3/exercice.js
@@ -1,8 +1,16 @@
 let libButton = document.getElementById('lib-button');
-let libIt = function() {
+let noun = document.getElementById("noun");
+let adjective = document.getElementById("adjective");
+let name = document.getElementById("person");
+
+
+
+function libIt() {
+
     let storyDiv = document.getElementById("story");
-    storyDiv.innerHTML = "??Your story here";
-};
+    storyDiv.innerHTML = name.value + " est " + adjective.value + " et a dans ca main un " + noun.value;
+}
+
 libButton.addEventListener('click', libIt);
 
 
